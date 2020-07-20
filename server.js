aeabff3b16111bae41c3c02b79c200ce63d4ccc8
Joshua Spence <joshua-spence@outlook.com>
2020-07-20 10:11:48 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+//get home page
+app.get('/index', (req, res) => {
+    res.render('index');
+});
+
+//get funeral page
+app.get('/funeral', (req, res) => {
+    res.render('funeral');
+});
+
+//get bespoke page
+app.get('/bespoke', (req, res) => {
+    res.render('bespoke');
+});
+
+//get wedding page
+app.get('/wedding', (req, res) => {
+    res.render('wedding');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('server connected');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
