2670486b2e1d5d5bc9d8f0dd07e94c503c128964
Joshua Spence <joshua-spence@outlook.com>
2020-07-20 11:13:18 +0100

    Changed port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
@@ -49,7 +50,7 @@ app.get('/wedding', (req, res) => {
 
 
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(PORT, () => {
     console.log('server connected');
 });
 
